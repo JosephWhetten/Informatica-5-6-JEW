22ad1de069c9e5149b38c2d3cd77e883f8980e32
JosephWhetten <joewhetten21408@gmail.com>
2025-08-13 19:38:19 +0000

    modifying hello.py with inputs

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello, world");
+print("Whatup fool")
+x = input("What's your name bro?\n")
+print("Aiight bet",x,"what's up")
+x = input("Aiight now give me a number\n")
+num = float(x)*4
+print(round(num , 0))
