42c4d3d7a7815d30ce15e07bb999e1d299218bb7
JosephWhetten <joewhetten21408@gmail.com>
2025-08-14 17:16:06 +0000

    finished indoor.py and playback.py

---
+++ indoor.py
@@ -0,0 +1,2 @@
+x = input("What is something that you hate when Coach Brandon says?\n").strip().lower()
+print(f"Oh you hate when he says {x} huh? WELL TOO BAD")
---
+++ playback.py
@@ -0,0 +1,3 @@
+x = input("What's your favorite color?\n")
+x = x.lstrip(' ').replace(" ","...")
+print(f"Calm down: {x}")
