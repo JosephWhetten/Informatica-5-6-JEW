84f2d7985d3cd1b97781d4f5ed6ea12dd47d8c3c
JosephWhetten <joewhetten21408@gmail.com>
2025-09-01 19:40:19 +0000

    doing adv-strings.py

--- Checkpoint 3/life-loop.py
+++ Checkpoint 3/life-loop.py
@@ -9,19 +9,34 @@ def main():
     sp = "purple"
     sgo = "gold"
     spi = "pink"
-    majiger = [sg, sb,sbl, sy, sp, sgo, spi]
+    majiger = [sg, sb, sbl, sy, sp, sgo, spi]
 
-    rem = 2
+    rem = 7
     while rem > 0:
         sock = input(f"What type of sock do you want? {majiger} ")
         rem -= 1
+        if sock == sg:
+            majiger = majiger - sg
+        if sock == sb:
+            majiger = [sg, sbl, sy, sp, sgo, spi]
+        if sock == sbl:
+            majiger = [sg, sb, sy, sp, sgo, spi]
+        if sock == sy:
+            majiger = [sg, sb, sbl, sp, sgo, spi]
+        if sock == sp:
+            majiger = [sg, sb, sbl, sy, sgo, spi]
+        if sock == sgo:
+            majiger = [sg, sb, sbl, sy, sp, spi]
+        if sock == spi:
+            majiger = [sg, sb, sbl, sy, sp, sgo]
 
     day = 7
     while day > 0:
+        time.sleep(2)
         socks = input("Good morning! What socks do you want to use? = ")
         print(f"You selected the {socks} socks, have a good day!\n")
         day -= 1
-        time.sleep(2)
+        
     print("You ran out of socks, time to wash the laundry!")
 
 main()
