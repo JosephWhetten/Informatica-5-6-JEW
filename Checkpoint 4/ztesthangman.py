14b7e0a356d82ea69b6722fde035d6f2913fdaa7
JosephWhetten <joewhetten21408@gmail.com>
2025-09-17 20:21:48 +0000

    finished for-intro

--- Checkpoint 4/ztesthangman.py
+++ Checkpoint 4/ztesthangman.py
@@ -57,11 +57,17 @@ def main():
         i += 1
     hidden_word = "_"
     blank_word = hidden_word*len(word)
+    a = 0
+    list = []
+    while a < len(word):
+        list.append("_")
+        a += 1
+    print(list)
     x = 0
     while x < len(word):
         guess = input("Guess = ")
         if guess in word:
-            word.remove[word.find[guess]]
-            word.insert[word.find[guess],guess]
+            list.remove(word.find(guess))
+            list.insert(word.find(guess),guess)
             print(blank_word)
 main()
