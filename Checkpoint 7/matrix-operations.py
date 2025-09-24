48a8e1edcabaad0efcecdc39c1ea7227222ff46e
JosephWhetten <joewhetten21408@gmail.com>
2025-09-24 20:20:48 +0000

    continued working on matrix-operations.py

--- Checkpoint 7/matrix-operations.py
+++ Checkpoint 7/matrix-operations.py
@@ -4,11 +4,20 @@ def main():
     for row in matrix:
         print(row)
     sum_row(matrix[int(input("Enter the row you want to sum = "))-1])
+    sum_column(int(input("Enter the column you want to sum = "))-1,matrix)
+    change(input("What element do you want to change? (x,y) = "),matrix)
 
 def sum_row(row):
     print(sum(row))
 
-def sum_column(column):
-    print(column)
+def sum_column(column,matrix):
+    column_sum = 0
+    for row in matrix:
+        column_sum += row[column]
+    print(column_sum)
+
+def change(element,matrix):
+    for row in matrix(list(element.replace(",","").split())[0]):
+        print(row)
 
 main()
