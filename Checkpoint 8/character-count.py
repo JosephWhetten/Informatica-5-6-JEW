b68d7e8aa9fa3a67a7c1bd22d76fea445cfa6e04
JosephWhetten <joewhetten21408@gmail.com>
2025-10-01 20:20:02 +0000

    finished character-count.py

--- Checkpoint 8/character-count.py
+++ Checkpoint 8/character-count.py
@@ -12,11 +12,12 @@ def character_counter(word):
     #     list.append(word[i])
     #     dictionary[word[i]] = list.count(word[i])
     #     i += 1
-    for character in word:
+    for character in word.replace(" ",""):
         dictionary.setdefault(character, 0)
         dictionary[character] += 1
     print(dictionary)
-    print(f"There are {len(word)} characters in the word")
-    print(max(dictionary.values()))
+    print(f"There are {len(word.replace(" ",""))} characters in the message")
+    print(f"The most repeated letter is {list(dictionary.keys())[list(dictionary.values()).index(max(dictionary.values()))]}")
+                                        #List of keys[(List of values).index(Most repeated letter)]
 
 main()
