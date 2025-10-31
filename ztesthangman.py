fbda27e6f641f20865cc1e48ffc794ab67753b6b
JosephWhetten <joewhetten21408@gmail.com>
2025-10-31 17:29:54 +0000

    workng on testman :)

--- ztesthangman.py
+++ ztesthangman.py
@@ -40,12 +40,13 @@
 #       |
 #  _____|___
 
-#    ___ 
-#    |  |
-#       |
-#       |
-#       |
-#  _____|___
+hangm0 ='''
+                ___ 
+                |  |
+                   |
+                   |
+                   |
+              _____|___'''
 
 def main():
 
@@ -55,43 +56,34 @@ def main():
     blank = list(hidden_word*len(word))
     print(blank)
 
-    realword = []
+    testword = []
     for element in word:
-        realword.append(element)
-    print(realword)
+        testword.append(element)
+    print(testword)
     
+    print(hangm0)
+    wrong_letter = []
     i = 0
     while i < len(word):
         guess = input("Type a letter: ")
-        if guess in realword:
+        if len(guess) > 1:
+            print("Only enter one letter")
+            print(type(guess))
+        elif guess in blank:
+            print("You already used this letter")
+        elif guess in testword:
             
-            for count in range(0, realword.count(guess)):
-                blank.pop(realword.index(guess))
-                blank.insert(realword.index(guess),guess)
-                realword.insert(realword.index(guess),"_")
-                realword.pop(realword.index(guess))
+            for count in range(0, testword.count(guess)):
+                blank.pop(testword.index(guess))
+                blank.insert(testword.index(guess),guess)
+                testword.insert(testword.index(guess),"_")
+                testword.pop(testword.index(guess))
                 i += 1
             print(blank)
-            print(realword)
+            print(testword)
+        else:
+            wrong_letter.append(guess)
+            print(wrong_letter)
+            print(len(wrong_letter))
             
-            
-
-    # for letter in word:
-    #     guess = input("Type in a letter = ")
-    #     if guess in word:
-    #         c = 0
-    #         while c < word.count(guess):
-    #             blank.pop(word.index(guess))
-    #             list(word).pop(word.index(guess))
-
-
-    # i = 0
-    # while i < len(word):
-    #     guess = input("Type in a letter =  ")
-    #     if guess in word:
-    #         blank.pop(word.index(guess))
-    #         blank.insert(word.index(guess),guess)
-    #         print(blank)
-    #         i += 1
-
 main()
