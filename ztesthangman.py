572d06f39b62eda1943f258f8e4827b52fe9651a
JosephWhetten <joewhetten21408@gmail.com>
2025-11-04 16:40:04 +0000

    finished emojize.py and working on hangman.py

--- ztesthangman.py
+++ ztesthangman.py
@@ -1,89 +1,99 @@
-#    ___ 
+#     ___ 
 #    |  |
 #    O  |
 #   /|\ |
 #   / \ |
 #  _____|___
 
-#    ___ 
+#     ___ 
 #    |  |
 #    O  |
 #   /|\ |
 #   /   |
 #  _____|___
 
-#    ___ 
+#     ___ 
 #    |  |
 #    O  |
 #   /|\ |
 #       |
 #  _____|___
 
-#    ___ 
+#     ___ 
 #    |  |
 #    O  |
 #   /|  |
 #       |
 #  _____|___
 
-#    ___ 
+#     ___ 
 #    |  |
 #    O  |
 #    |  |
 #       |
 #  _____|___
 
-#    ___ 
+#     ___ 
 #    |  |
 #    O  |
 #       |
 #       |
 #  _____|___
 
-hangm0 ='''
-                ___ 
-                |  |
-                   |
-                   |
-                   |
-              _____|___'''
+hangman0 ='''
+          ___ 
+         |  |
+            |
+            |
+            |
+       _____|___'''
+
+import getpass
 
 def main():
 
-    word = input("What is the word going to be? = ")
+    word = getpass.getpass("What is the word going to be? = ")
 
     hidden_word = "_"
-    blank = list(hidden_word*len(word))
-    print(blank)
+    correct_guess = list(hidden_word*len(word))
+    print(correct_guess)
 
     testword = []
     for element in word:
         testword.append(element)
     print(testword)
     
-    print(hangm0)
-    wrong_letter = []
+    print(hangman0)
+    
+    wrong_guess = []
     i = 0
     while i < len(word):
         guess = input("Type a letter: ")
-        if len(guess) > 1:
+        # Tests for integers
+        if guess.isdigit():
+            print("You must enter a letter")
+        # Tests if for than one letter
+        elif len(guess) > 1:
             print("Only enter one letter")
             print(type(guess))
-        elif guess in blank:
+        # Test if you already have it
+        elif guess in correct_guess or guess in wrong_guess:
             print("You already used this letter")
+        # Test if you already guessed it
+        
         elif guess in testword:
             
             for count in range(0, testword.count(guess)):
-                blank.pop(testword.index(guess))
-                blank.insert(testword.index(guess),guess)
+                correct_guess.pop(testword.index(guess))
+                correct_guess.insert(testword.index(guess),guess)
                 testword.insert(testword.index(guess),"_")
                 testword.pop(testword.index(guess))
                 i += 1
-            print(blank)
+            print(correct_guess)
             print(testword)
         else:
-            wrong_letter.append(guess)
-            print(wrong_letter)
-            print(len(wrong_letter))
+            wrong_guess.append(guess)
+            print(wrong_guess)
+            print(len(wrong_guess))
             
 main()
